c11b49f126e4ca8515aedda20b24992999b74a26
onbat2 <quangduc11202@gmail.com>
2022-12-11 17:54:56 +0700

    [UPDATE]

--- J01016_CHU_SO_4_VA_CHU_SO_7/src/j01016_chu_so_4_va_chu_so_7/J01016_CHU_SO_4_VA_CHU_SO_7.java
+++ J01016_CHU_SO_4_VA_CHU_SO_7/src/j01016_chu_so_4_va_chu_so_7/J01016_CHU_SO_4_VA_CHU_SO_7.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package j01016_chu_so_4_va_chu_so_7;
+import java.util.Scanner;
 
-/**
- *
- * @author Duke
- */
 public class J01016_CHU_SO_4_VA_CHU_SO_7 {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner m_sc = new Scanner(System.in);
+        String s = m_sc.nextLine();
+        int d = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '4' || s.charAt(i) == '7'){
+                d++;
+            }
+        }
+        if(d == 4 || d == 7){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
     }
     
 }
